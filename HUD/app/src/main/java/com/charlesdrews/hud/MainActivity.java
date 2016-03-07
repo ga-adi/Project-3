8dc44e9b6b40d671ceb54b9c7156c37ad11782da
William-Tygret <wctygret@gmail.com>
2016-03-07 15:48:16 -0600

    innitial will commit, working on retrofit

--- HUD/app/src/main/java/com/charlesdrews/hud/MainActivity.java
+++ HUD/app/src/main/java/com/charlesdrews/hud/MainActivity.java
@@ -11,6 +11,10 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    String mNYTimesURL = "http://api.nytimes.com/svc/news/v3/content/all/all/4.json?limit=20&api-key=9d6920a74c817405b74a87b98db7a4da%3A4%3A74605150";
+    String mSearchNYTimesUrl = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q="+search+"&page=1&sort=newest&api-key=29975101513df1dfdf9895c3324ca6d2:6:74605150";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
