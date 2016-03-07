9b4ade933ae8992fde295715201e5199d00c8551
charlesdrews <charlesdrews@gmail.com>
2016-03-07 12:36:43 -0500

    Add base class for recycler adapter view holders

--- HUD/app/src/main/java/com/charlesdrews/hud/RecyclerAdapter.java
+++ HUD/app/src/main/java/com/charlesdrews/hud/RecyclerAdapter.java
@@ -6,31 +6,38 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.charlesdrews.hud.HudCardData.CardData;
+import com.charlesdrews.hud.HudCardData.CardType;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * Add cards to the recycler view
  * Created by charlie on 3/7/16.
  */
-public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.CardViewHolder> {
     private List<CardData> mCardsData;
+    private ArrayList<CardType> mCardTypes;
 
     public RecyclerAdapter(List<CardData> cardsData) {
         mCardsData = cardsData;
+        mCardTypes = new ArrayList<>(Arrays.asList(CardType.values()));
     }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public CardViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        CardType type = mCardTypes.get(viewType);
         View view = null;
+
         //TODO - inflate the correct layout for each possible CardType value
-        switch (viewType) {
-            case 0: // Weather
+        switch (type) {
+            case Weather:
                 //view = LayoutInflater.from(parent.getContext()).inflate(R.layout.weather_card, parent, false);
-                return new WeatherCard(view);
-            case 1: // Twitter
+                return new WeatherCard(view, type);
+            case Twitter:
                 //view = LayoutInflater.from(parent.getContext()).inflate(R.layout.twitter_card, parent, false);
-                return new TwitterCard(view);
+                return new TwitterCard(view, type);
             default:
                 return null;
         }
@@ -38,27 +45,16 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
 
     @Override
     public int getItemViewType(int position) {
-        //TODO - pick an int for each possible CardType value
-        switch (mCardsData.get(position).getType()) {
-            case Weather:
-                return 0;
-            case Twitter:
-                return 1;
-            case News:
-                return 2;
-            case Concerts:
-                return 3;
-            default:
-                return -1;
-        }
+        CardType type = mCardsData.get(position).getType();
+        return mCardTypes.indexOf(type);
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(CardViewHolder holder, int position) {
         CardData data = mCardsData.get(position);
 
         //TODO - bind data to views for each possible CardType value
-        switch (data.getType()) {
+        switch (holder.getCardType()) {
             case Weather:
                 return;
             case Twitter:
@@ -75,16 +71,29 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
         return mCardsData.size();
     }
 
-    //TODO - extend ViewHolder for each possible CardType value
-    public class WeatherCard extends RecyclerView.ViewHolder {
-        public WeatherCard(View itemView) {
+    public class CardViewHolder extends RecyclerView.ViewHolder {
+        private CardType mCardType;
+
+        public CardViewHolder(View itemView, CardType cardType) {
             super(itemView);
+            mCardType = cardType;
+        }
+
+        public CardType getCardType() {
+            return mCardType;
         }
     }
 
-    public class TwitterCard extends RecyclerView.ViewHolder {
-        public TwitterCard(View itemView) {
-            super(itemView);
+    //TODO - extend ViewHolder for each possible CardType value
+    public class WeatherCard extends CardViewHolder {
+        public WeatherCard(View itemView, CardType cardType) {
+            super(itemView, cardType);
+        }
+    }
+
+    public class TwitterCard extends CardViewHolder {
+        public TwitterCard(View itemView, CardType cardType) {
+            super(itemView, cardType);
         }
     }
 }
