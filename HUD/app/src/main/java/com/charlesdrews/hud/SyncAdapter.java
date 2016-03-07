ed58c18133fbfe1cc7d66c44ec64ff8fb7c5f9a7
charlesdrews <charlesdrews@gmail.com>
2016-03-07 10:10:52 -0500

    Add boilerplate syncadapter, service, content provider code

---
+++ HUD/app/src/main/java/com/charlesdrews/hud/SyncAdapter.java
@@ -0,0 +1,31 @@
+package com.charlesdrews.hud;
+
+import android.accounts.Account;
+import android.content.AbstractThreadedSyncAdapter;
+import android.content.ContentProviderClient;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.SyncResult;
+import android.os.Bundle;
+
+/**
+ * Created by charlie on 3/7/16.
+ */
+public class SyncAdapter extends AbstractThreadedSyncAdapter {
+    ContentResolver mContentResolver;
+
+    public SyncAdapter(Context context, boolean autoInitialize) {
+        super(context, autoInitialize);
+        mContentResolver = context.getContentResolver();
+    }
+
+    public SyncAdapter(Context context, boolean autoInitialize, boolean allowParallelSyncs) {
+        super(context, autoInitialize, allowParallelSyncs);
+        mContentResolver = context.getContentResolver();
+    }
+
+    @Override
+    public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
+        //TODO - use Retrofit to make API calls & parse responses
+    }
+}
