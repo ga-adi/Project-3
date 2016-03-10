cc5c0bb56dbc02d89d16abc262e8baf85f309361
William-Tygret <wctygret@gmail.com>
2016-03-10 11:15:51 -0600

    working on anim... not working so far lol

--- HUD/app/src/main/java/com/charlesdrews/hud/RecyclerAdapter.java
+++ HUD/app/src/main/java/com/charlesdrews/hud/RecyclerAdapter.java
@@ -1,9 +1,12 @@
 package com.charlesdrews.hud;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import com.charlesdrews.hud.CardsData.CardData;
@@ -23,6 +26,7 @@ import java.util.List;
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.CardViewHolder> {
     private final ArrayList<CardType> mCardTypes = new ArrayList<>(Arrays.asList(CardType.values()));
     private List<CardData> mCardsData;
+
 
 
     public RecyclerAdapter(List<CardData> cardsData) {
@@ -62,6 +66,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.CardVi
     @Override
     public void onBindViewHolder(CardViewHolder holder, int position) {
         CardData data = mCardsData.get(position);
+
 
         //TODO - bind data to views for each possible CardType value
         switch (holder.getCardType()) {
@@ -140,4 +145,5 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.CardVi
             mHeadline = (TextView) itemView.findViewById(R.id.headline);
         }
     }
+
 }
