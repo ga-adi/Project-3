995cd4fc49ebf6f42eca8148ad0141ae54035fb5
charlesdrews <charlesdrews@gmail.com>
2016-03-10 11:54:32 -0500

    Make news items clickable - link out to browser

--- HUD/app/src/main/java/com/charlesdrews/hud/CardsData/NewsRecyclerAdapter.java
+++ HUD/app/src/main/java/com/charlesdrews/hud/CardsData/NewsRecyclerAdapter.java
@@ -1,11 +1,14 @@
 package com.charlesdrews.hud.CardsData;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.charlesdrews.hud.R;
@@ -30,13 +33,19 @@ public class NewsRecyclerAdapter extends RecyclerView.Adapter<NewsRecyclerAdapte
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        NewsCardData.NewsItemData item = mData.get(position);
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        final NewsCardData.NewsItemData item = mData.get(position);
 
         holder.mHeadline.setText(item.getHeadline());
-        //TODO - set thumbnail using url
         Picasso.with(holder.mContext).load(item.getThumbnailUrl()).into(holder.mThumbnail);
         //TODO - make item clickable w/ link url
+        holder.mContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri linkUrl = Uri.parse(item.getLinkUrl());
+                holder.mContext.startActivity(new Intent(Intent.ACTION_VIEW, linkUrl));
+            }
+        });
     }
 
     @Override
@@ -46,12 +55,14 @@ public class NewsRecyclerAdapter extends RecyclerView.Adapter<NewsRecyclerAdapte
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         Context mContext;
+        RelativeLayout mContainer;
         ImageView mThumbnail;
         TextView mHeadline;
 
         public ViewHolder(Context context, View itemView) {
             super(itemView);
             mContext = context;
+            mContainer = (RelativeLayout) itemView.findViewById(R.id.newsItemContainer);
             mThumbnail = (ImageView) itemView.findViewById(R.id.newsItemThumbnail);
             mHeadline = (TextView) itemView.findViewById(R.id.newsItemHeadline);
         }
