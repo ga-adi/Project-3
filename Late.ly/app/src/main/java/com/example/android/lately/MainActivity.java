8359005519cf1a0f5ae2895ba458f922d45a6e4f
WasabiMayo <hyewon910119@gmail.com>
2016-03-09 19:55:17 -0500

    Weather API call implemented

--- Late.ly/app/src/main/java/com/example/android/lately/MainActivity.java
+++ Late.ly/app/src/main/java/com/example/android/lately/MainActivity.java
@@ -1,32 +1,71 @@
 package com.example.android.lately;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.android.lately.Forecast.Weather;
 import com.example.android.lately.Fragments.DetailsFragment;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
     Window mWindow;
     Toolbar mMainToolbar;
     boolean mPortrait;
+
+    private static String mForecastUrl = "https://api.forecast.io/forecast/39a42687f8dbe7c14cd4f97d201af744/";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        mGoogleApiClient.connect();
 
         mWindow = this.getWindow();
         mWindow.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
@@ -191,7 +230,61 @@ public class MainActivity extends AppCompatActivity {
             transaction.replace(R.id.detailsFragmentContainer, fragment);
             transaction.commit();
         }
+        }
+
+
+    public void getForecastApi(){
+        String latitude = String.valueOf(mLastLocation.getLatitude()).substring(0, 7);
+        String longitude = String.valueOf(mLastLocation.getLongitude()).substring(0, 8);
+        String latlon = latitude+","+longitude;
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(mForecastUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        WeatherRequest weatherRequest = retrofit.create(WeatherRequest.class);
+        Call<Weather> result = weatherRequest.getWeather(latlon);
+        result.enqueue(new Callback<Weather>() {
+            @Override
+            public void onResponse(Call<Weather> call, Response<Weather> response) {
+
+                //Getting current weather status
+
+                String currentTemperature = String.valueOf(response.body().getCurrently().getTemperature());
+                String currentSummary = response.body().getCurrently().getSummary();
+                String currentLocation = response.body().getTimezone();
+
+                SimpleDateFormat format = new SimpleDateFormat("MMM dd, yyyy hha z, EEE");
+                format.setTimeZone(TimeZone.getTimeZone("GMT"+response.body().getOffset()));
+                Date currentDate = new Date((long)(response.body().getCurrently().getTime() * 1000L));
+                String formattedCurrentDate = format.format(currentDate);
+
+
+                //Getting next five days' weather
+                String[] nextFiveDaysDates = new String[5];
+                String[] nextFiveDaysSummary = new String[5];
+                String[] nextFiveDaysHighTemp = new String[5];
+                String[] nextFiveDaysLowTemp = new String[5];
+                for(int i=0; i<5; i++){
+                    Date nextDayTime = new Date((long)(response.body().getDaily().getData().get(i+1).getTime() * 1000L));
+                    String formattedDate = format.format(nextDayTime);
+                    nextFiveDaysDates[i] = formattedDate;
+                    //If you want to just extract day names(i.e. Tue), comment out the line above this and uncomment the below one.
+                    //nextFiveDaysDates[i] = formattedDate.substring(formattedDate.length()-3,formattedDate.length());
+
+                    nextFiveDaysSummary[i] = response.body().getDaily().getData().get(i+1).getSummary();
+                    nextFiveDaysHighTemp[i] = String.valueOf(response.body().getDaily().getData().get(i+1).getTemperatureMax());
+                    nextFiveDaysHighTemp[i] = String.valueOf(response.body().getDaily().getData().get(i+1).getTemperatureMin());
+                }
+            }
+            @Override
+            public void onFailure(Call<Weather> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -199,6 +292,69 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                Toast.makeText(MainActivity.this, "Please allow this app to access to your location", Toast.LENGTH_SHORT).show();
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        0);
+            }
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            if (mLastLocation != null) {
+                getForecastApi();
+//                    getRedditApi();
+//                    getFoursquareApi();
+//                    getMeetupApi();
+            } else {
+                Toast.makeText(MainActivity.this, "Turn on GPS and try again", Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            Toast.makeText(MainActivity.this, "No network connection", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    public interface WeatherRequest{
+        @GET("{latlon}/")
+        Call<Weather> getWeather(@Path("latlon") String latlon);
+    }
 //    @Override
 //    public boolean onOptionsItemSelected(MenuItem item) {
 //        // Handle action bar item clicks here. The action bar will
