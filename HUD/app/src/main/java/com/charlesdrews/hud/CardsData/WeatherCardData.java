34f36b947b8a279c987e9165d8e0c5b586d1445a
William-Tygret <wctygret@gmail.com>
2016-03-10 09:24:55 -0600

    layout designing

--- HUD/app/src/main/java/com/charlesdrews/hud/CardsData/WeatherCardData.java
+++ HUD/app/src/main/java/com/charlesdrews/hud/CardsData/WeatherCardData.java
@@ -7,6 +7,8 @@ public class WeatherCardData extends CardData {
     //TODO - feel free to change this around - this was just for testing
     private int mHighTemp, mLowTemp;
 
+
+
     public WeatherCardData(CardType type, int highTemp, int lowTemp) {
         super(type);
         mHighTemp = highTemp;
